af8c2aab7b13601ee9b2dab8757b2fb23e8f53d7
Horst Chuck <162436484+luciaechler@users.noreply.github.com>
2024-03-06 11:58:43 +0200

    Create brand.js

---
+++ brand.js
@@ -0,0 +1,24 @@
+var match = this.rows.find((row) => {
+			return row.data === data;
+		});
+		
+		return match || false;
+	}
+
+return new Promise((resolve, reject)=>{
+			if(renderInPosition && this.getDisplayRows().length){
+				if(this.table.options.pagination){
+					this._setDataActual(data, true);
+				}else{
+					this.reRenderInPosition(() => {
+						this._setDataActual(data);
+					});
+				}
+        }else{
+				if(this.table.options.autoColumns && columnsChanged && this.table.initialized){
+					this.table.columnManager.generateColumnsFromRowData(data);
+				}
+				this.resetScroll();
+				
+				this._setDataActual(data);
+			}
